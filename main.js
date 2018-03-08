ff9374a19ea0d187eb599163fb20a76fe29305fe
Eric Fischer <efischer@edx.org>
2018-03-08 14:02:50 -0500

    moar

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 #!/usr/bin/env node
 var fs = require('fs');
 
-var data = JSON.parse(fs.readFileSync(process.argv[2], 'utf8'));
-console.log("data parsed without error")
+var inData = JSON.parse(fs.readFileSync(process.argv[2], 'utf8'));
+
+var outData = {}
+inData.forEach((message) => {
+  outData[message["id"]] = message["defaultMessage"];
+});
+fs.writeFile(process.argv[3], JSON.stringify(outData, null, 2), 'utf8');
