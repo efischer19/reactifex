9fe7349635ae5c5039e1eb7ecb1b12e1950a1e71
Eric Fischer <efischer@edx.org>
2018-03-08 15:29:25 -0500

    lint stuff (#7)

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
 #!/usr/bin/env node
-const path = require('path');
-const fs = require('fs');
+const path = require("path");
+const fs = require("fs");
 
-combineInputs = (dir) => {
+var combineInputs = (dir) => {
   // expected input: a directory containing subdirs, each of which contains displayMessages.json
   var ret = [];
-  subDirs = fs.readdirSync(dir);
+  var subDirs = fs.readdirSync(dir);
   subDirs.forEach((subdir) => {
-    messageFile = path.join(dir, subdir, 'displayMessages.json');
-    messages = JSON.parse(fs.readFileSync(messageFile));
+    var messageFile = path.join(dir, subdir, "displayMessages.json");
+    var messages = JSON.parse(fs.readFileSync(messageFile));
     messages.forEach((message) => {ret.push(message);});
   });
   return ret;
-}
+};
 var inData = combineInputs(process.argv[2]);
 
-var outData = {}
+var outData = {};
 inData.forEach((message) => {
   outData[message["id"]] = message["defaultMessage"];
 });
