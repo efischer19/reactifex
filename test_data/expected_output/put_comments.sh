ae76475adffb6966f4a342bc9fcea1675dacff0c
Eric Fischer <efischer@edx.org>
2018-04-02 16:23:59 -0400

    Make put_comments.sh static, data can be dynamic (#10)
    Also update tests to illustrate what's happening now.

--- test_data/expected_output/put_comments.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-curl -L -w "\n" --user $SECRET_USER:$SECRET_PWD -X PUT -H "Content-Type: application/json" --data "{\"comment\": \"It's a message, for testing\"}" $112345deadbeef
-curl -L -w "\n" --user $SECRET_USER:$SECRET_PWD -X PUT -H "Content-Type: application/json" --data "{\"comment\": \"Also a message for testing\"}" $18badf00d
-curl -L -w "\n" --user $SECRET_USER:$SECRET_PWD -X PUT -H "Content-Type: application/json" --data "{\"comment\": \"It's a message, for testing\"}" $1bada55
-curl -L -w "\n" --user $SECRET_USER:$SECRET_PWD -X PUT -H "Content-Type: application/json" --data "{\"comment\": \"Also a message for testing\"}" $1feedface9876
